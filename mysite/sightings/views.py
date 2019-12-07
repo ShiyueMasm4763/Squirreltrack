056ac867c370c49cf64b3ec59bc1578780c63f7a
Shiyue Ma <sm4763@columbia.edu>
2019-12-07 21:07:31 +0000

    write views.py

--- mysite/sightings/views.py
+++ mysite/sightings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from .forms import SightingsForm
+from .models import Squirrel
 
-# Create your views here.
+
+
+def display(request,*args,**kwargs):
+
+    list1 = Squirrel.objects.all()
+    list2 = ['Unique_Squirrel_ID','Location','Date']
+
+    context={
+        'squirrels':list1,
+        'fields':list2,
+    }    
+
+    return render(request,'sightings/sightings_list.html',context)
+
