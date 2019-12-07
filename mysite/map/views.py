14e8ae61666b1407b3cfcf72f304879b8ba5c39c
Jia Zelang <jialang@s-MacBook-Pro.lan1>
2019-12-07 15:23:34 -0500

    Finished map parts, including templates, view.py and urls.py

--- mysite/map/views.py
+++ mysite/map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from sightings.models import Squirrel
+
+def map(request,*args,**kwargs):
+    sightings = Squirrel.objects.all()[:20]
+    context={'sightings':sightings}     
+    return render(request,'map/map.html',context)from django.shortcuts import render
+
 # Create your views here.
