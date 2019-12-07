88c04f5f7067a8ef224e42b46628ce16c70dac89
Jia Zelang <jialang@s-MacBook-Pro.lan1>
2019-12-07 16:03:08 -0500

    modify the urls.py in mysite and sightings

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sightings.views import sightings_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('map/', include('map.urls')),
+    path('sightings/', include('sightings.urls')),
 ]
