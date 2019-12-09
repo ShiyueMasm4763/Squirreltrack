922da5d654976e5493c0bd32470aa220fabf04b0
Shiyue Ma <sm4763@columbia.edu>
2019-12-09 01:39:35 +0000

    Attempt for CSS

--- mysite/sightings/management/commands/export_squirrel_data.py
+++ mysite/sightings/management/commands/export_squirrel_data.py
@@ -9,9 +9,9 @@ class Command(BaseCommand):
     def handle(self,*args,**kwargs):
     
     with open('export_squirrel_data.csv',mode='w') as csvfile:
-        set2 = csv.writer(csvfile,delimiter = ',')
-        all_fields = [field1.name for field1 in Squirrel._meta.get_fields()]
-        fieldnames = [Squirrel._meta.get_field(field2).help_text for field2 in all_fields[1:]]
+        writer = csv.writer(csvfile,delimiter = ',')
+        all_fields = [a.name for a in Squirrel._meta.get_fields()]
+        fieldnames = [Squirrel._meta.get_field(b).help_text for b in all_fields[1:]]
         writer.writerow(fieldnames)
         for i in range(len(Squirrel.objects.all())):
             rowval = []
