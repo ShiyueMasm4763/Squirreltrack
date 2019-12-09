4875d8836712d2c195520759bffb40a492ee11bd
Jia Zelang <jialang@s-MacBook-Pro.lan1>
2019-12-08 19:03:38 -0500

    changed html

--- mysite/sightings/urls.py
+++ mysite/sightings/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
         path('',views.display,name='sightings'),
-        path('stats/', views.stats, name='stats'),
         path('add/',views.add, name='add'),
+        path('stats/', views.stats, name='stats'),
         path('<str:squirrel_id>',views.modify,name='modify'),
         ]
